ceb6746d4b462a8e6979011729876f1bf9e05e69
Travis Lobert <travis.lobert@wellsky.com>
2021-02-03 15:53:00 -0700

    added setupEnvVar.sh to make is easier to run from terminal

---
+++ setupEnvVar.sh
@@ -0,0 +1,6 @@
+export ASPNETCORE_Environment=Development &&
+export AWS_PROFILE=fred &&
+export AWS_REGION=us-east-1 &&
+echo ASPNETCORE_Environment=$ASPNETCORE_Environment &&
+echo AWS_PROFILE=$AWS_PROFILE &&
+echo AWS_REGION=$AWS_REGION
