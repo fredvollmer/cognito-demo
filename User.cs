fc7b11b540eec99e15bdff951324d2e2197f5645
Fred Vollmer <fred.vollmer@wellsky.com>
2021-02-05 09:54:57 -0500

    ClaimsPrincipal to User extension

--- User.cs
+++ User.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +17,7 @@ namespace cognito_dotnet_angular
     }
 
     public class User
+    
     {
         public string Id { get; set; }
         public string Email { get; set; }
@@ -46,13 +50,40 @@ namespace cognito_dotnet_angular
                 
                 // Role claim has a specific type that allows it to be used in statements such as 
                 //  [Authorize(Role = "admin")]
+                // We also include in a namespaced claim. This is needed for conversion.
                 new Claim(ClaimTypes.Role, this.Role),
+                new Claim(getClaimName(nameof(Role)), this.Role),
             };
         }
 
         private string getClaimName(string name)
         {
             return $"applicationuser:{name}";
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static User ApplicationUser(this ClaimsPrincipal principal)
+        {
+            // Ensure an application user has been stored on this ClaimsPrincipal
+            if (!principal.HasClaim(c => c.Type == "applicationuser:Id"))
+            {
+                throw new InvalidOperationException("ClaimsPrincipal does not have required claims for conversion to an Application User");
+            }
+
+            var claims = principal.Claims.ToDictionary(c => c.Type);
+
+            return new User
+            {
+                Id = claims["applicationuser:Id"].Value,
+                Email = claims["applicationuser:Email"].Value,
+                FirstName = claims["applicationuser:FirstName"].Value,
+                LastName = claims["applicationuser:LastName"].Value,
+                TerriotyId = claims["applicationuser:TerritoryId"].Value,
+                Role = claims["applicationuser:Role"].Value,
+                IsEnabled = bool.Parse(claims["applicationuser:IsEnabled"].Value),
+            };
         }
     }
 }
